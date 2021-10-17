800138820927249dedc8c3bd5bffc63f0c07d4bf
vicchern <victor.cherniiavskiy@securenettech.com>
2021-10-17 08:20:36 +0300

    AWS temporary setup. Added config class

---
+++ src/main/java/com/vicchern/fileuploaderapp/bucket/BucketName.java
@@ -0,0 +1,14 @@
+package com.vicchern.fileuploaderapp.bucket;
+
+public enum BucketName {
+
+    PROFILE_IMAGE("myfilesupload");
+
+
+    private String bucketName;
+
+    BucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+}
